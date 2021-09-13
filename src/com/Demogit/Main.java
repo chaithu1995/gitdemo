a2b8fae0506215af9e2f4fe4cc9d8eb2e13e94e0
dev2 <saichaitanya969@gmail.com>
2021-09-13 16:07:07 +0530

    first demo

---
+++ src/com/Demogit/Main.java
@@ -0,0 +1,9 @@
+package com.Demogit;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("This is my first git demo");
+    }
+}
